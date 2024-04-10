3b64a58869bd4950ce6f16483cf8b50e8b29ffd9
dimedical-corporativo <redessociales@dimedicalcorporativo.mx>
2024-04-09 18:47:11 -0600

    feat(task): tasks kanban

--- di-medical-client/src/pages/task/GlobalTasksPage.vue
+++ di-medical-client/src/pages/task/GlobalTasksPage.vue
@@ -1,112 +1,88 @@
 <template>
   <div class="column flex flex-center ">
-    <div class="tasks q-mt-md">
+    <div class="tasks q-mt-lg">
       <div class="task-column bg-grey-2 shadow-2">
-        <div class="task-title bg-primary flex flex-center">
+        <div class="task-title bg-primary column justify-center">
+          <p class="q-ml-md text-subtitle1">Backlog</p>
         </div>
 
-        <div class="task-list">
+        <div class="task-list q-mt-md">
           <q-card class="task-card" flat bordered v-for="i in 6" :key="i">
             <q-card-section horizontal>
               <q-card-section class="q-pt-xs">
-                <div class="text-overline">Overline</div>
                 <div class="text-h5 q-mt-sm q-mb-xs">Title</div>
                 <div class="text-caption text-grey">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </div>
-              </q-card-section>
-      
-              <q-card-section class="col-5 flex flex-center">
-                <q-img
-                  class="rounded-borders"
-                  src="https://cdn.quasar.dev/img/parallax2.jpg"
-                />
               </q-card-section>
             </q-card-section>
       
             <q-separator />
       
             <q-card-actions>
-              <q-btn flat round icon="event" />
               <q-btn flat>
                 7:30PM
               </q-btn>
               <q-btn flat color="primary">
-                Reserve
+                Editar
               </q-btn>
             </q-card-actions>
           </q-card>
         </div>
       </div>
       <div class="task-column bg-grey-2 shadow-2">
-        <div class="task-title bg-primary flex flex-center">
+        <div class="task-title bg-primary column justify-center">
+          <p class="q-ml-md text-subtitle1">Backlog</p>
         </div>
 
-        <div class="task-list">
+        <div class="task-list q-mt-md">
           <q-card class="task-card" flat bordered v-for="i in 6" :key="i">
             <q-card-section horizontal>
               <q-card-section class="q-pt-xs">
-                <div class="text-overline">Overline</div>
                 <div class="text-h5 q-mt-sm q-mb-xs">Title</div>
                 <div class="text-caption text-grey">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </div>
-              </q-card-section>
-      
-              <q-card-section class="col-5 flex flex-center">
-                <q-img
-                  class="rounded-borders"
-                  src="https://cdn.quasar.dev/img/parallax2.jpg"
-                />
               </q-card-section>
             </q-card-section>
       
             <q-separator />
       
             <q-card-actions>
-              <q-btn flat round icon="event" />
               <q-btn flat>
                 7:30PM
               </q-btn>
               <q-btn flat color="primary">
-                Reserve
+                Editar
               </q-btn>
             </q-card-actions>
           </q-card>
         </div>
       </div>
       <div class="task-column bg-grey-2 shadow-2">
-        <div class="task-title bg-primary flex flex-center">
+        <div class="task-title bg-primary column justify-center">
+          <p class="q-ml-md text-subtitle1">Backlog</p>
         </div>
 
-        <div class="task-list">
+        <div class="task-list q-mt-md">
           <q-card class="task-card" flat bordered v-for="i in 6" :key="i">
             <q-card-section horizontal>
               <q-card-section class="q-pt-xs">
-                <div class="text-overline">Overline</div>
                 <div class="text-h5 q-mt-sm q-mb-xs">Title</div>
                 <div class="text-caption text-grey">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </div>
-              </q-card-section>
-      
-              <q-card-section class="col-5 flex flex-center">
-                <q-img
-                  class="rounded-borders"
-                  src="https://cdn.quasar.dev/img/parallax2.jpg"
-                />
               </q-card-section>
             </q-card-section>
       
             <q-separator />
       
             <q-card-actions>
-              <q-btn flat round icon="event" />
               <q-btn flat>
                 7:30PM
               </q-btn>
               <q-btn flat color="primary">
-                Reserve
+                Editar
               </q-btn>
             </q-card-actions>
           </q-card>
@@ -125,6 +101,7 @@
   .tasks {
     width: 100%;
     height: 100%;
+    margin-bottom: 10px;
     display: flex;
     justify-content: center;
 
@@ -133,14 +110,13 @@
       margin-left: 15px;
     }
 
-    overflow: scroll;
-    gap: 240px;
+    gap: 270px;
     .task {
       
       &-column {
         flex: 0 0 auto;
         width: 390px;
-        height: 90vh;
+        min-height: 90vh;
       } 
 
       &-title {
@@ -149,12 +125,12 @@
       }
 
       &-list {
-        height: 90%;
+        min-height: 90%;
         display: flex;
         align-items: center;
         flex-direction: column;
-        overflow: scroll;
         gap: 15px;
+        margin-bottom: 15px;
       }
 
       &-card {
